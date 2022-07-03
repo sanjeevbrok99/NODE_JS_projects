37442a34cc8d1add0034965767c6fe1a05b50393
Sanjeev Mishra <mishragsanjeev1999@gmail.com>
2022-07-03 19:50:03 +0530

    sending bulk email through sendgrid

---
+++ mailsender.ts
@@ -0,0 +1,29 @@
+const http = require("http")
+const port = 3000;
+http.createServer((req,res)=>{
+
+	const mailer = require("@sendgrid/mail");
+
+	mailer.setApikey("x34fkiednfkgnfdjrjkir")
+
+	const message = {
+		to: ["mishragsaneev1999@gmail.com"],
+		from: "helloworld@gmail.com",
+		subject: "This is for demo purpose",
+		html: "<h1> New messege from Sanjeev Mishra</h1>",
+		"<p> some demo text from sanjeev mishra </p> "		
+
+
+	}
+	mailer.send(message,function(err,json){
+		if (err){
+			res.write("message cant be sent")
+		}
+		else{
+			res.write("mail sent to Sanjeev Mishra")
+		}
+	})
+		res.end()																																							
+})
+
+.listen(port,()=>{console.log(`server is running on port 3000`)})
